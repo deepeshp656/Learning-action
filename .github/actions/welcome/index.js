9d01b5ae289e6f7a1627131953c5f5e1e353a402
Deepesh Patel <Deepeshp656@gmail.com>
2020-12-25 04:20:29 -0500

    Update index.js

--- .github/actions/welcome/index.js
+++ .github/actions/welcome/index.js
@@ -11,8 +11,8 @@ try {
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   let student = JSON.parse(payload);
   console.log(student.action);
-  console.log(student);
-  //console.log(`The event payload: ${payload["action"]}`);
+  console.log(student.pull_request.Labels);
+  console.log(`The event payload: ${payload["action"]}`);
 } catch (error) {
   core.setFailed(error.message);
 }
