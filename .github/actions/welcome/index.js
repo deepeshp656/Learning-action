e7fedfbf1055d03091aebf9a64749934b35d44c1
Deepesh Patel <Deepeshp656@gmail.com>
2020-12-25 03:48:28 -0500

    Update index.js

--- .github/actions/welcome/index.js
+++ .github/actions/welcome/index.js
@@ -8,7 +8,7 @@ try {
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = toJson(github)
+  const payload = github.context.payload;
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
