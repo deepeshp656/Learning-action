5a9159c7ba4526a358294d31c5aad7a36ef2e380
Deepesh Patel <Deepeshp656@gmail.com>
2020-12-25 03:32:29 -0500

    Update index.js

--- .github/actions/welcome/index.js
+++ .github/actions/welcome/index.js
@@ -3,13 +3,13 @@ const github = require('@actions/github');
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
+ // const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello Deepesh`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  console.log(`The event payload: ${payload[Labels]}`);
 } catch (error) {
   core.setFailed(error.message);
 }
